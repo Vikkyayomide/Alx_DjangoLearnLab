a9b3b37381d84c703680a023e4ada4f3b143aff4
vikky <olawunmi886@gmail.com>
2025-09-30 18:33:04 +0100

    html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -43,7 +43,6 @@ from django.shortcuts import render
 def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
